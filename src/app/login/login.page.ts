4fa10295b8934206497a79cb47a7d9f6871e4b72
CarolinaT <27895429@ifts24.edu.ar>
2025-06-26 14:55:15 -0300

    modal

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, Validator, Validators } from '@angular/forms';
 import {IonicModule} from '@ionic/angular';
 import { AuthService } from '../services/auth.service';
+import { ToastController, ModalController } from '@ionic/angular';
 
 
 @Component({
@@ -16,6 +17,8 @@ export class LoginPage {
 
   private fb=inject(FormBuilder)
   private auth=inject(AuthService)
+  private toastController = inject(ToastController);
+  private modalController = inject(ModalController);
 
     loginForm = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
@@ -23,8 +26,6 @@ export class LoginPage {
   })
 
   passwordVisible= false;
-  login_error=false;
-  mensajeError='';
 
   togglePassword() {
     this.passwordVisible = !this.passwordVisible
@@ -36,36 +37,86 @@ export class LoginPage {
 
       try {
         await this.auth.iniciarSesion(email!, password!)
+        this.mostrarToast('Inicio de sesión exitoso', 'success');
       } catch (error: any) {
-        this.login_error=true
-        this.mensajeError = error.message || 'Error desconocido';
-        console.log(error);
+      const mensaje = this.traducirError(error.code);
+      this.mostrarToast(mensaje, 'danger');
+      console.error(error);
 
-         // Ocultar mensaje de error después de 5 segundos
-    setTimeout(() => {
-      this.login_error = false;
-      }, 5000);
+      
 
       }
     }else {
       this.loginForm.markAllAsTouched();
-      alert('Formualrio invalido')
+      this.mostrarToast('Formulario inválido', 'danger');
     }
   }
 
   // Registrar usuario
-  async registrar() {
+  /*async registrar() {
     if(this.loginForm.valid) {
       const {email, password} = this.loginForm.value
 
       try {
         await this.auth.registrar(email!, password!)
       } catch (error: any) {
-        alert('Error al intentar registrar: '+ error.message)
+        this.mostrarToast('Error al intentar registrar: ' + error.message, 'danger');
+    }
+  }*/
+
+  async mostrarToast(mensaje: string, color: string = 'warning') {
+  const toast = await this.toastController.create({
+    message: mensaje,
+    duration: 3000,
+    position: 'bottom',
+    color: color
+  });
+
+  await toast.present();
+}
+async registrar() {
+  if (this.loginForm.valid) {
+    const { email, password } = this.loginForm.value;
+
+    try {
+      await this.auth.registrar(email!, password!);
+      this.mostrarToast('Usuario registrado con éxito', 'success');
+
+    } catch (error: any) {
+      if (error.code === 'auth/email-already-in-use') {
+        this.mostrarToast('El usuario ya existe', 'warning');
+      } else {
+        this.mostrarToast('Error al registrar: ' + error.message, 'danger');
       }
     }
+  } else {
+    this.loginForm.markAllAsTouched();
+    this.mostrarToast('Formulario inválido', 'danger');
   }
+}
 
-  
+private traducirError(code: string): string {
+  switch (code) {
+    case 'auth/user-not-found':
+      return 'Usuario no registrado. Verifica tu email.';
+    case 'auth/wrong-password':
+      return 'Contraseña incorrecta. Intentá de nuevo.';
+    case 'auth/invalid-email':
+      return 'El email no es válido.';
+    case 'auth/invalid-credential':
+      return 'Email o contraseña incorrectos.';
+    case 'auth/missing-password':
+      return 'Falta ingresar la contraseña.';
+    case 'auth/email-already-in-use':
+      return 'El usuario ya existe.';
+    default:
+      return 'Ocurrió un error inesperado.';
+  }
+}
+
+ionViewWillEnter() { // metodo que limpia automaticamente cada vez que la vista esta por entrar
+  this.loginForm.reset();
+  this.passwordVisible = false;
+}
 
 }
